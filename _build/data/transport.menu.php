c58675b0f88b433b34fe17ed55ec4a1e37a80cff
Oetzie <info@oetzie.nl>
2017-05-03 21:24:28 +0200

    Icon fixed

--- _build/data/transport.menu.php
+++ _build/data/transport.menu.php
@@ -17,7 +17,7 @@
 	    'namespace' 	=> PKG_NAME_LOWER,
 	    'parent' 		=> 'components',
 	    'description' 	=> PKG_NAME_LOWER.'.desc',
-	    'icon' 			=> '',
+	    'icon' 			=> 'comments',
 	    'menuindex' 	=> 0,
 	    'params' 		=> '',
 	    'handler' 		=> '',
