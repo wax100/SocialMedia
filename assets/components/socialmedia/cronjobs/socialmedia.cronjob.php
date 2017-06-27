a18fb7370ffbedecd831b717877734cbef7b0fab
Oetzie <info@oetzie.nl>
2017-06-27 17:09:28 +0200

    Version update

--- assets/components/socialmedia/cronjobs/socialmedia.cronjob.php
+++ assets/components/socialmedia/cronjobs/socialmedia.cronjob.php
@@ -19,7 +19,7 @@
 	 * Suite 330, Boston, MA 02111-1307 USA
 	 */
 
-	require_once dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))).'/config.core.php';
+	require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/config.core.php';
 	
 	require_once MODX_CORE_PATH.'model/modx/modx.class.php';
 	
